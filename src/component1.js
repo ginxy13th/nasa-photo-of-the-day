29167593d303ff3e3efb10e948651a356da0dda0
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-07-09 23:20:42 -0400

    got keys and basic layout, but problem with hooks

--- src/component1.js
+++ src/component1.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const component1 = ({props}) => {
+const component1 = ({title, date, hdurl}) => {
     return (
-        <div>
-            placeholder
+        <div class= 'component1'>
+           <h3>Title: {title}</h3>
+           <p>Date: {date}</p> 
+            <img alt="PictureOfTheDay">{hdurl} </img>
         </div>
     )
 }
--- src/makerpage.js
+++ src/makerpage.js
@@ -3,9 +3,33 @@ import axios from "axios";
 import component1 from "./component1.js";
 
 export default function makerpage() {
+    const [property, setProperty] = useState([]);
+
+    useEffect(() => {
+        axios
+        //  .get('https://api.nasa.gov/planetary/apod')
+        .then(response => {
+            console.log(response);
+            setProperty(response.data);
+        })
+        .catch(error => {
+            console.log('error:', error)
+        })
+    }, []);
+
     return (
-        <div>
-            <component1 />
-        </div>
+        <div class='componentmaker'>
+            {property.map(eachProperty => {
+              return (
+              <component1 
+                key = {eachProperty.id}
+                title = {eachProperty.title}
+                date = {eachProperty.date}
+                hdurl = {eachProperty.hdurl}
+                /> 
+                    ) 
+                })
+            }
+       </div> 
     )
 }
