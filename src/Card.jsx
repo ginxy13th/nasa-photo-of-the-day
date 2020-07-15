e8954c5f5b163876bead29406c5acf03178b8495
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-07-14 20:35:59 -0400

    added styles

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import {Card} from 'reactstrap';
+import styled from 'styled-components';
 
+const insidewrapperDiv = styled.div`
+display:flex;
+justify-content:center;
+`;
 
-
-const Card = ({img, title, date}) => {
+const p = styled.p`
+background:blue;
+color:white;
+`;
+const Cards = ({img, title, date}) => {
     return (
-        <div class='card'>
-        <img alt='space of the day'src={img}></img>
-           <h3>{title}</h3>
-           <p>{date}</p>
-        </div>
+    
+    <Card body inverse style={{ backgroundColor: 'blue', color: 'white' }} class='card'>
+        <insidewrapperDiv>
+            <img width='80%' alt='space of the day'src={img}></img>
+            <p>{title}</p>
+            <p>{date}</p>
+            </insidewrapperDiv>
+        </Card>
     )
     }
-export default Card;
+export default Cards;
