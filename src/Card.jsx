b15dc0798d0d8bb35e4c5385ad2fafce8649b9d9
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-07-16 14:22:01 -0400

    adding calender for changable dates

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Card} from 'reactstrap';
 import styled from 'styled-components';
-
+import Calender from './datepicker.jsx'
 const insidewrapperDiv = styled.div`
 display:flex;
 justify-content:center;
@@ -11,17 +11,19 @@ display:flex;
 justify-content:center;
 `;
 
-const Cards = ({ img, title, copyright, date, explaination }) => {
+const Cards = ({ img, title, date, copyright, explaination }) => {
     return (
     
     <Card body inverse style={{ backgroundColor: 'royalblue', color: 'white' }} class='card'>
-        <imageDiv>
+         <h1>NASA PHOTO OF THE DAY</h1>
+         <Calender />
+         <imageDiv>
             <img width='80%' alt='space of the day'src={img}></img>
         </imageDiv>
         <insidewrapperDiv body inverse style={{ backgroundColor: 'blue', color: 'white' }}>
             <p>{title}</p>
-            <p>{copyright}</p>
             <p>{date}</p>
+            <p>{copyright}</p>
             <p>{explaination}</p>
          </insidewrapperDiv>
     </Card>
--- src/Makerpage.jsx
+++ src/Makerpage.jsx
@@ -1,15 +1,19 @@
 import React, { useState, useEffect} from "react";
 import Card from './Card';
 import axios from "axios";
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 
 
 const Makerpage = () => {
+    
     const[data, setData]= useState([]);
-
+    const [date, setDate] = useState([])
+    
     useEffect(() => {
         axios
-         .get('https://api.nasa.gov/planetary/apod?api_key=CZtOXA5O8YsycSSTTSj53338NDbMBdhvjAF8yNs4&date=2020-07-14')
+         .get(`https://api.nasa.gov/planetary/apod?api_key=CZtOXA5O8YsycSSTTSj53338NDbMBdhvjAF8yNs4&date=${date}`)
         .then(response => {
             console.log(response.data)
               setData([response.data])
@@ -17,7 +21,7 @@ const Makerpage = () => {
         .catch(error => {
             console.log('error:', error)
         })
-    }, [])
+    }, [date])
 
     return( 
     <div className='makerpage-container'>
@@ -25,10 +29,10 @@ const Makerpage = () => {
             return (
              <Card 
              key={p.id} 
+             date={p.date}
              img={p.url}
              title={p.title}
              copyright={p.copyright}
-             date={p.date}
              explaination={p.explanation}/>   
             ) 
         }
